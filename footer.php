12b4d70ef0b21fc34bdae00d18f8646ec486a063
Emmanuele Somma <exedre@gmail.com>
2014-03-18 13:17:23 +0100

    aggiunta la "firma" in calce alla pagina"

--- footer.php
+++ footer.php
@@ -20,6 +20,9 @@
     </a> and <a href="<?php echo esc_url(__('http://renniaofei.com/freebies/bestcorporate/', 'bestcorporate')); ?>" title="<?php esc_attr_e('Best Corporate Theme', 'bestcorporate'); ?>" target="_blank">
     <?php _e('Best Corporate Theme', 'bestcorporate'); ?>
     </a></div>
+  <div id="site-maker">
+    <?php _e('Il nuovo sito dei seminari è stato realizzato da ','bestcorporate')?><a href="mailto:emmanuele.somma@bancaditalia.it"><?php _e('E. Somma','bestcorporate')?></a><?php _e(' nel Febbraio 2014 (per maggiori informazioni ','bestcorporate')?><a href="/cs/?page_id=105"><?php _e('Sviluppo Decentrato','bestcorporate')?></a><?php _e(')','bestcorporate')?>
+  </div> 
 </footer>
 <!-- #colophon -->
 </div>
